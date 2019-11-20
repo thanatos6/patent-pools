f5459a2bc93d0345576eed3826671a1e2cbae9e6
Zhang-LeYing <894843410@qq.com>
2019-11-20 14:45:50 +0800

    在配置文件中增加扫描mapper.xmml

--- core/src/main/java/com/suixingpay/Main.java
+++ core/src/main/java/com/suixingpay/Main.java
@@ -1,9 +1,11 @@
 package com.suixingpay;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@MapperScan("com.suixingpay.mapper")
 public class Main {
     public static void main(String[] args) {
         SpringApplication.run(Main.class, args);
