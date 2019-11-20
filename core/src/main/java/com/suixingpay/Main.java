254dff38431121e755588ad76ab6e9fa892ed9c9
hyx <hyx960722@163.com>
2019-11-20 14:33:17 +0800

    hyx

--- core/src/main/java/com/suixingpay/Main.java
+++ core/src/main/java/com/suixingpay/Main.java
@@ -1,4 +1,4 @@
-package com.suixingpay.common;
+package com.suixingpay;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
---
+++ core/src/main/java/com/suixingpay/controller/TestController.java
@@ -0,0 +1,14 @@
+package com.suixingpay.controller;
+
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+
+    @RequestMapping("/hello")
+    public String hello(){
+        return "hello spring";
+    }
+}
