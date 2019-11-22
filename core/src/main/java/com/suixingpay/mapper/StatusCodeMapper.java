474b94eb371662d5a25132a3ee06db92a4cf1b82
Zhang-LeYing <894843410@qq.com>
2019-11-22 11:21:06 +0800

    修改流程状态

--- core/src/main/java/com/suixingpay/mapper/StatusCodeMapper.java
+++ core/src/main/java/com/suixingpay/mapper/StatusCodeMapper.java
@@ -1,8 +1,11 @@
 package com.suixingpay.mapper;
 
 import com.suixingpay.pojo.PatentInfo;
+import com.suixingpay.pojo.StatusCode;
 import org.apache.ibatis.annotations.Mapper;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 /**
  * @author zhangleying
@@ -12,11 +15,18 @@ import org.springframework.stereotype.Repository;
 @Mapper
 public interface StatusCodeMapper {
     void updateStatusPass(int patentId);
-    void updateStatusReject (int patentId);
-    void updateStatusWriter (int patentId);
+
+    void updateStatusReject(int patentId);
+
+    void updateStatusWriter(int patentId);
+
     //点击认领
-    void updateStatusClaim (int patentId);
+    void updateStatusClaim(PatentInfo patentInfo);
+
     //点击编辑完成
-    void updateStatusFinish (int patentId);
+    void updateStatusFinish(int patentId);
+
+    //根据角色码查找有权限的待办
+    List<StatusCode> selectCodeByRole(int role);
 }
 
