6760c8080d8a4ad6290d544dcb5958dd6788347b
jiefeimadi <865033445@qq.com>
2019-12-05 11:19:57 +0800

    登陆修改

--- core/src/main/java/com/suixingpay/controller/LoginController.java
+++ core/src/main/java/com/suixingpay/controller/LoginController.java
@@ -1,22 +1,22 @@
 package com.suixingpay.controller;
 
 
-        import com.github.pagehelper.util.StringUtil;
-        import com.suixingpay.pojo.User;
-        import com.suixingpay.service.Impl.UserImpl;
-        import com.suixingpay.service.UserService;
-        import com.suixingpay.util.ZhuanliUtil;
-        import org.slf4j.Logger;
-        import org.slf4j.LoggerFactory;
-        import org.springframework.beans.factory.annotation.Autowired;
-        import org.springframework.util.StringUtils;
-        import org.springframework.web.bind.annotation.*;
+import com.github.pagehelper.util.StringUtil;
+import com.suixingpay.pojo.User;
+import com.suixingpay.service.Impl.UserImpl;
+import com.suixingpay.service.UserService;
+import com.suixingpay.util.ZhuanliUtil;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.util.StringUtils;
+import org.springframework.web.bind.annotation.*;
 
-        import javax.annotation.Resource;
-        import javax.servlet.http.HttpServletRequest;
-        import javax.servlet.http.HttpSession;
-        import javax.xml.ws.Action;
-        import java.util.Date;
+import javax.annotation.Resource;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+import javax.xml.ws.Action;
+import java.util.Date;
 
 @RestController
 public class LoginController {
