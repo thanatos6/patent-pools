f8e72c3a3d55fc5243b4902a4da73f22089bef44
kongjian <298029530@qq.com>
2019-12-07 11:20:19 +0800

    写测试controller实验全局异常处理和response类

--- miaosha/src/main/java/com/suixingpay/controller/WelcomeController.java
+++ miaosha/src/main/java/com/suixingpay/controller/WelcomeController.java
@@ -4,6 +4,7 @@ import com.suixingpay.pojo.CodeEnum;
 import com.suixingpay.pojo.Response;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -24,6 +25,15 @@ public class WelcomeController {
         String hello = "hello kongjian";
         Map<String, Object> result = new HashMap<>();
         result.put("msg", hello);
+        Response<Map<String, HashMap>> response = Response.getInstance(CodeEnum.SUCCESS, result);
+        return response;
+    }
+
+    @RequestMapping("/exc")
+    @ResponseBody
+    public Response exc(@RequestParam("name") String name) {
+        Map<String, Object> result = new HashMap<>();
+        result.put("name", name);
         Response<Map<String, HashMap>> response = Response.getInstance(CodeEnum.SUCCESS, result);
         return response;
     }
